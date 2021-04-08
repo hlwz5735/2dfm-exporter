6b80e54995edc903cde2439faf651b0958059805
limeng <limengwwwq@163.com>
2021-04-08 16:20:50 +0800

    fix: 精灵帧表因数据量较大而在切换时出现卡顿

--- src/renderer/views/player/SpriteFrameTable.vue
+++ src/renderer/views/player/SpriteFrameTable.vue
@@ -1,7 +1,7 @@
 <template>
   <a-layout style="height: calc(100vh - 64px)">
     <a-layout-sider theme="light" style="height: 100%; overflow: auto">
-      <a-list bordered :data-source="player.spriteFrames">
+      <a-list bordered :data-source="notEmptySprites">
         <a-list-item slot="renderItem" slot-scope="item, index">
           {{ index }} - {{ item.width }} * {{ item.height }}
         </a-list-item>
@@ -17,6 +17,7 @@
 
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import _2DFMPlayer from '@/entity/2dfm-player'
+import _2DFMSpriteFrame from '@/entity/2dfm-sprite-frame'
 @Component({
   name: 'SpriteFrameTable',
   components: {}
@@ -26,6 +27,10 @@ export default class SpriteFrameTable extends Vue {
     required: true
   })
   player: _2DFMPlayer
+
+  get notEmptySprites(): Array<_2DFMSpriteFrame> {
+    return this.player.spriteFrames.filter(val => val.width * val.height !== 0)
+  }
 }
 </script>
 
